524c003439c79999c76d519b328494f26ecb0dd1
salimkhandev <salimt450s@gmail.com>
2024-10-08 21:23:21 +0500

    added

--- app/page.js
+++ app/page.js
@@ -1,29 +1,26 @@
-// "use client"
-
 import React from "react";
-import lets from "../hooks/useGetQuery"
+import lets from "../hooks/useGetQuery";
 import Link from "next/link";
 
 async function Page(props) {
-  console.log(props.searchParams,"props");
-  
-  // throw new err
-  const data= await lets()
-  // console.log(data,"data")
+  console.log(props.searchParams, "props");
 
+  // Fetching data
+  const data = await lets();
 
-  return <div className="h-full">
-
-<ul>
-  {data.map((item, index)=>(
-
-    <li key={index}>
-      <Link href={`/blogpost/${item.id}`}> <a>{item.name}</a></Link>
-    </li>
-
-  ))}
-</ul>
-  </div>;
+  return (
+    <div className="h-full bg-gray-100 p-4"> {/* Added background and padding */}
+      <ul className="space-y-4"> {/* Space between list items */}
+        {data.map((item, index) => (
+          <li key={index} className="bg-white shadow-md p-4 rounded-md"> {/* List item styling */}
+            <Link href={`/blogpost/${item.id}`} className="text-blue-500 hover:underline"> {/* Link styling */}
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Page;
